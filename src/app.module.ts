b34495bbe15b378a046a6714e4d1f152102d79f7
Yusuf Eliaçık <yusuf@fireflyon.com>
2022-04-07 17:22:00 +0300

    deleted env helper

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { getEnvPath } from './common/helper/env.helper';
 import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
 import { ApiModule } from './api/api.module';
-
-const envFilePath: string = getEnvPath(`${__dirname}`);
-console.log(envFilePath);
 
 @Module({
   imports: [
